3c430284b587dada9079ed48d874e383a724a8d1
Ratn Govindam <88680132+RG-7@users.noreply.github.com>
2024-01-16 09:38:45 +0530

    Question 8 (a) completed.

---
+++ Assignment_1/Question_8.py
@@ -0,0 +1,15 @@
+# Question 8
+# (a)
+class Resturant:
+  def __init__(self,resturant_name,cuisine_type):
+    self.resturant_name = resturant_name
+    self.cuisine_type = cuisine_type
+  def describe_resturant(self):
+    print(f'Resutrant Name: - {self.resturant_name}\nCuisine Type - {self.cuisine_type}')
+  def open_resturant(self):
+    print("The resturant is open!")
+
+res = Resturant("The Street Cafe","Indian")
+res.describe_resturant()
+res.open_resturant()
+
