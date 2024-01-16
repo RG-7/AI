b70c5ec8a3b8d6af836dcd32e52ffebe0d65d313
Ratn Govindam <88680132+RG-7@users.noreply.github.com>
2024-01-16 11:19:55 +0530

    Question 8 Completed!

--- Assignment_1/Question_8.py
+++ Assignment_1/Question_8.py
@@ -17,3 +17,20 @@ print(res.resturant_name)
 print(res.cuisine_type)
 res.describe_resturant()
 res.open_resturant()
+
+# (b)
+class User:
+  def __init__(self,first_name, last_name,email,reg_no,deptt,sub_group):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.email = email
+    self.reg_no = reg_no
+    self.deptt = deptt
+    self.sub_group = sub_group
+  def describe_User(self):
+    print(f"Name: {self.first_name +' '+ self.last_name}\nReg. No.: {self.reg_no}\nDeptt.: {self.deptt}\nSub-Group: {self.sub_group}\nEmail: {self.email}")
+  def greet_User(self):
+    print(f"Hi {self.first_name + ' ' + self.last_name}, Welcome to the TheStreetCafe!!😊🧡")
+u = User("Ratn","Govindam","rg.ratn@gmail.com",102397020,"CSE","CS-10")
+u.describe_User()
+u.greet_User()
