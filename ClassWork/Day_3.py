ea3527e847e644cd525a772015476f4ae490f275
Ratn Govindam <88680132+RG-7@users.noreply.github.com>
2024-01-23 09:38:09 +0530

    Update Day_3.py

--- ClassWork/Day_3.py
+++ ClassWork/Day_3.py
@@ -1,25 +1,43 @@
-# Day 3
+# Day 2
 # January 23, 2024
-# 8 Puzzle Problem
-# Not completed Yet
+# Assignment 2
 
 # Question 1
 
-mylist = []
+mylist_initial = []
+mylist_goal = []
 
 for row in range(0, 3):
     temp = []
     for col in range(0, 3):
         temp.append(input())
-    mylist.append(temp)
+    mylist_initial.append(temp)
 
-for lst in mylist:
+for lst in mylist_initial:
+    print(lst)
+
+for row in range(0, 3):
+    temp = []
+    for col in range(0, 3):
+        temp.append(input())
+    mylist_goal.append(temp)
+
+for lst in mylist_goal:
     print(lst)
 
 def search_index(mylist, number):
     for i in range(0, 3):
         for j in range(0, 3):
-            if mylist[i][j] == number:
+            if mylist[i][j] == str(number):
                 return [i, j]
 
-print(search_index(mylist, 0))
+
+def move_up(mat):
+  l = search_index(mat,0)
+  row = l[0]
+  col = l[1]
+  if row !=0:
+    mat[row][col],mat[row-1][col] =mat[row-1][col] ,mat[row][col]
+    return mat
+  else:
+    return
